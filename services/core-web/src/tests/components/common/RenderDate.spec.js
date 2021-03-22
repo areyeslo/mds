cec5a04701d03ed0f75e5e4d96ebe901e8231e2d
Luke Mitchell <lukegamitchell@gmail.com>
2021-03-22 15:38:12 -0700

    Update RenderDate.spec.js

--- services/core-web/src/tests/components/common/RenderDate.spec.js
+++ services/core-web/src/tests/components/common/RenderDate.spec.js
@@ -1,31 +1,32 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import RenderDate from '@/components/common/RenderDate';
+import React from "react";
+import { shallow } from "enzyme";
+import RenderDate from "@/components/common/RenderDate";
 
 let props = {};
 
 const setupProps = () => {
   props = {
     id: 1,
-    input: '',
-    label: '',
-    placeholder: 'yyyy-mm-dd',
+    input: {
+      onChange: () => {},
+    },
+    label: "",
+    placeholder: "yyyy-mm-dd",
     onChange: jest.fn((date) => date),
-    onBlur: jest.fn(() => {}),
     meta: {
       touched: false,
       error: false,
       warning: false,
     },
   };
-}
+};
 
 beforeEach(() => {
   setupProps();
 });
 
-describe('RenderDate', () => {
-  it('renders properly', () => {
+describe("RenderDate", () => {
+  it("renders properly", () => {
     const wrapper = shallow(<RenderDate {...props} />);
     expect(wrapper).toMatchSnapshot();
   });
