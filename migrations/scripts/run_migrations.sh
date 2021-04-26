0f1e8461a9730703a65f9589da4fab84582affb4
jon-funk <johnthomasfunk@gmail.com>
2021-04-26 10:13:24 -0700

    Update flyway migrations to be AWS compatible

--- migrations/scripts/run_migrations.sh
+++ migrations/scripts/run_migrations.sh
@@ -18,6 +18,3 @@ export PATH="$FLYWAY_HOME:$PATH"
 
 # Run migrations for mds database
 flyway migrate
-
-# Run migrations for mds_test database
-flyway -url=jdbc:postgresql://$FLYWAY_DB_HOST/mds_test migrate
